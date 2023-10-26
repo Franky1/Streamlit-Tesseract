1a41c0301365b6f7a38b0b9931ecb0cc7a2c73fb
Ivan Kovalev <ivan.kovalev@mbrus.ru>
2023-10-26 13:40:34 +0300

    Refactored tesseract library functionality

--- helpers/tesseract.py
+++ helpers/tesseract.py
@@ -1,15 +1,123 @@
 import shutil
 import streamlit as st
+import pytesseract
 
 
-# search for tesseract binary in path
+pytesseract.pytesseract.tesseract_cmd = None
+
+# set tesseract path
+@st.cache_resource
+def set_tesseract_path(tesseract_path: str):
+    pytesseract.pytesseract.tesseract_cmd = tesseract_path
+
+# Search for Tesseract binary in path
 @st.cache_resource
 def find_tesseract_binary() -> str:
     return shutil.which("tesseract")
 
+# Get Tesseract version
+@st.cache_resource
+def get_tesseract_version():
+    # check if tesseract is installed
+    try:
+        tesseract_version = pytesseract.get_tesseract_version()
+        return tesseract_version
+    except pytesseract.TesseractNotFoundError:
+        st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract.")
+        st.stop()
+    except Exception as e:
+        st.error("Unexpected Exception")
+        st.error(f"Error Message: {e}")
+        st.stop()
+    else:
+        if not tesseract_version:
+            st.error("Tesseract is not installed. Please install Tesseract.")
+            st.stop()
 
-# create custom oem and psm config string
+
+
+
+# Create custom OEM and PSM config string
 @st.cache_data
 def get_tesseract_config(oem_index: int, psm_index: int) -> str:
     custom_oem_psm_config = f'--oem {oem_index} --psm {psm_index}'
     return custom_oem_psm_config
+
+
+# Set Tesseract binary path and check installation
+@st.cache_data
+def set_tesseract_cmd_and_check_installation() -> str:
+    tesseract_cmd = find_tesseract_binary()
+    if not tesseract_cmd:
+        st.error("Tesseract binary not found in PATH. Please install Tesseract.")
+        return None
+    try:
+        tesseract_version = pytesseract.get_tesseract_version()
+        if not tesseract_version:
+            st.error("Tesseract is not installed. Please install Tesseract.")
+            return None
+    except pytesseract.TesseractNotFoundError:
+        st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract.")
+        return None
+    except Exception as e:
+        st.error("Unexpected Exception")
+        st.error(f"Error Message: {e}")
+        return None
+    return tesseract_cmd
+
+@st.cache_data
+def check_installed_languages(language_short, language):
+    try:
+        installed_languages = pytesseract.get_languages(config='')
+    except pytesseract.TesseractError:
+        st.error("TesseractError: Tesseract reported an error during language data extraction.")
+        st.stop()
+    except pytesseract.TesseractNotFoundError:
+        st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract.")
+        st.stop()
+    except Exception as e:
+        st.error("Unexpected Exception")
+        st.error(f"Error Message: {e}")
+        st.stop()
+    else:
+        if language_short not in installed_languages:
+            st.error(f'Selected language "{language}" is not installed. Please install language data.')
+            st.stop()
+        return installed_languages
+
+
+def extract_text_from_image(image, language_short, custom_oem_psm_config, timeout):
+    """
+    This function attempts to extract text from an image using Tesseract OCR.
+    It takes an image, a language code, a custom OEM and PSM config string, and a timeout value as input.
+    If any error occurs during the text extraction, an appropriate error message is displayed and the Streamlit app is stopped.
+
+    :param image: Image to extract text from
+    :param language_short: Short code for the language used in the image
+    :param custom_oem_psm_config: Custom configuration string for OEM and PSM
+    :param timeout: Timeout value for Tesseract OCR
+    :return: Extracted text if successful, None otherwise
+    """
+    try:
+        text = pytesseract.image_to_string(image=image,
+                                           lang=language_short,
+                                           output_type=pytesseract.Output.STRING,
+                                           config=custom_oem_psm_config,
+                                           timeout=timeout)
+        return text
+    except pytesseract.TesseractError as e:
+        st.error("TesseractError: Tesseract reported an error during text extraction.")
+        st.error(f"Error Message: {e}")
+        st.stop()
+    except pytesseract.TesseractNotFoundError:
+        st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract.")
+        st.stop()
+    except RuntimeError:
+        st.error("RuntimeError: Tesseract timed out during text extraction.")
+        st.stop()
+    except Exception as e:
+        st.error("Unexpected Exception")
+        st.error(f"Error Message: {e}")
+        st.stop()
+    return None
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,28 +10,10 @@ import helpers.pdfimage as pdfimage
 import helpers.tesseract as tesseract
 import helpers.easy_ocr as easy_ocr
 
-pytesseract.pytesseract.tesseract_cmd = None
-
-# set tesseract path
-@st.cache_resource
-def set_tesseract_path(tesseract_path: str):
-    pytesseract.pytesseract.tesseract_cmd = tesseract_path
 
 # streamlit config
-st.set_page_config(page_title="Tesseract OCR", page_icon="📝", layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(page_title="Local OCR", page_icon="📝", layout="wide", initial_sidebar_state="expanded")
 
-# apply custom css
-with open('helpers/style.css') as css:
-    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
-
-# add streamlit title
-st.title("Tesseract OCR - Optical Character Recognition 📝")
-
-# add streamlit markdown text
-# st.markdown('''**Tesseract OCR** - Optical Character Recognition using Tesseract, OpenCV and Streamlit.<br>
-# This is a simple OCR demo app that can be used to extract text from images. Supported languages see below.
-# ''', unsafe_allow_html=True)
-st.markdown(f'''# {constants.flag_string}''')
 
 # set tesseract binary path
 pytesseract.pytesseract.tesseract_cmd = tesseract.find_tesseract_binary()
@@ -39,23 +21,24 @@ if not pytesseract.pytesseract.tesseract_cmd:
     st.error("Tesseract binary not found in PATH. Please install Tesseract.")
     st.stop()
 
-# check if tesseract is installed
-try:
-    tesseract_version = pytesseract.get_tesseract_version()
-except pytesseract.TesseractNotFoundError:
-    st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract.")
-    st.stop()
-except Exception as e:
-    st.error("Unexpected Exception")
-    st.error(f"Error Message: {e}")
-    st.stop()
-else:
-    if not tesseract_version:
-        st.error("Tesseract is not installed. Please install Tesseract.")
-        st.stop()
+
+# apply custom css
+with open('helpers/style.css') as css:
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+
+# add streamlit title
+st.title("Local OCR - Optical Character Recognition 📝")
+
+# add streamlit markdown text
+# st.markdown('''**Local OCR** - Optical Character Recognition using Tesseract, OpenCV and Streamlit.<br>
+# This is a simple OCR demo app that can be used to extract text from images. Supported languages see below.
+# ''', unsafe_allow_html=True)
+st.markdown(f'''# {constants.flag_string}''')
+
+
 
 with st.sidebar:
-    st.success(f"Tesseract Version **{tesseract_version}** is installed.")
+    st.success(f"Tesseract Version **{tesseract.get_tesseract_version()}** is installed.")
     st.header("Tesseract OCR Settings")
     language = st.selectbox(label="Select Language", options=list(constants.languages_sorted.values()), index=constants.default_language_index)
     language_short = list(constants.languages_sorted.keys())[list(constants.languages_sorted.values()).index(language)]
@@ -91,23 +74,7 @@ psm_index = constants.psm.index(psm)
 custom_oem_psm_config = tesseract.get_tesseract_config(oem_index=oem_index, psm_index=psm_index)
 
 # check if installed languages are available
-installed_languages = list()
-try:
-    installed_languages = pytesseract.get_languages(config='')
-except pytesseract.TesseractError:
-    st.error("TesseractError: Tesseract reported an error during language data extraction.")
-    st.stop()
-except pytesseract.TesseractNotFoundError:
-    st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract..")
-    st.stop()
-except Exception as e:
-    st.error("Unexpected Exception")
-    st.error(f"Error Message: {e}")
-    st.stop()
-else:
-    if language_short not in installed_languages:
-        st.error(f'Selected language "{language}" is not installed. Please install language data.')
-        st.stop()
+installed_languages = tesseract.check_installed_languages(language_short, language)
 
 # two column layout for image preprocessing options and image preview
 col1, col2 = st.columns(spec=[2, 3], gap="large")
@@ -193,36 +160,26 @@ with col2:
             # streamlit spinner
             with st.spinner("Extracting Text..."):
                 try:
-                    # st.info(f"Tesseract configuration: {custom_oem_psm_config}")
-                    text = pytesseract.image_to_string(image=image,
-                                                lang=language_short,
-                                                output_type=pytesseract.Output.STRING,
-                                                config=custom_oem_psm_config,
-                                                timeout=timeout)
-                except pytesseract.TesseractError as e:
-                    st.error("TesseractError: Tesseract reported an error during text extraction.")
-                    st.error(f"Error Message: {e}")
-                    st.stop()
-                except pytesseract.TesseractNotFoundError:
-                    st.error("TesseractNotFoundError: Tesseract is not installed. Please install Tesseract..")
-                    st.stop()
-                except RuntimeError:
-                    st.error("RuntimeError: Tesseract timed out during text extraction.")
-                    st.stop()
-                except Exception as e:
-                    st.error("Unexpected Exception")
-                    st.error(f"Error Message: {e}")
-                    st.stop()
-                else:
-                    # add streamlit subheader
-                    # st.subheader("Extracted Text")
+                    # Assuming extract_text_from_image function is already defined and imported
+                    # Make sure to pass the required arguments to the function (e.g., image, language_short, custom_oem_psm_config, timeout)                    
+                    text = tesseract.extract_text_from_image(image, language_short, custom_oem_psm_config, timeout)
+                    print(text)
+                    
                     if text:
                         # TODO: move this to the whole page again
                         # TODO: try Ace Editor for text area instead
                         # add streamlit text area
-                        st.text_area(label="Extracted Text", value=text, height=500)
+                        st.text_area("Extracted Text", value=text, height=500, key="extracted_text")
+                        
                         # add streamlit download button for extracted text
-                        st.download_button(label="Download Extracted Text", data=text.encode("utf-8"), file_name="extract.txt", mime="text/plain")
+                        st.download_button("Download Extracted Text", data=text.encode("utf-8"), file_name="extract.txt", mime="text/plain", key="download_button")
                     else:
-                        st.warning("No text was extracted.")
+                        st.warning("No text was extracted. Please try again with a different image or settings.")
                         st.stop()
+                except Exception as e:
+                    st.error("An unexpected error occurred.")
+                    st.error(f"Error Message: {str(e)}")
+                    st.stop()
+
+
+
